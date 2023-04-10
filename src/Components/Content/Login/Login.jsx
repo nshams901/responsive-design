3e59f6fbe11752dfe732eb850e1c0d0bba0197e6
abhi808shek <abhi808gupta@gmail.com>
2023-04-10 23:05:24 +0530

    Linked Login and Singup Pages

--- src/Components/Content/Login/Login.jsx
+++ src/Components/Content/Login/Login.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom"; // import the Link component
 import Input from "../InputBox/Input";
 import Heading from "../Heading/Heading";
 import PasswordInput from "../InputBox/PasswordInput";
 import Button2 from "../Button/Button2";
-import {NavLink} from "react-router-dom"
 
 const Login = () => {
-
   return (
     <>
       <div className="w-full h-full rounded-[20px] flex flex-col justify-center items-center gap-2">
@@ -20,12 +19,18 @@ const Login = () => {
           errorMessage="Password is incorrect. Please re-enter."
         />
         <div className="w-full">
-          <p className="ml-8 text-xs font-bold mb-2"> Forget Password ?</p>
+          <Link to="/newpassword" className="ml-8 text-xs font-bold mb-2">
+            Forget Password ?
+          </Link>
         </div>
-        <Button2 title="Sign In" bgColor="#7991BD"/>
+        <Button2 title="Sign In"/>
         <p className="text-xs font-bold text-[#7B8FA1]">
-          Don't have account ? <span className="text-[#7991BD]">Sign Up</span>
-        </p>
+        Don't have account ? 
+        <Link to="/signup" className="text-[#7991BD]">
+           Sign Up
+        </Link>
+      </p>
+
       </div>
     </>
   );
