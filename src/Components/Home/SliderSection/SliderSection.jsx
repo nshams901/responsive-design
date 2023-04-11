dfba24387d4cbe1ca473225377cf520046a34c7a
abhi808shek <abhi808gupta@gmail.com>
2023-04-11 23:45:34 +0530

    Home Page

--- src/Components/Home/SliderSection/SliderSection.jsx
+++ src/Components/Home/SliderSection/SliderSection.jsx
@@ -12,57 +12,52 @@ const SliderSection = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
+      items: 2,
     },
   };
   return (
-    <div className="w-full h-[220px] flex justify-center bg-red-300">
-      <div className="flex flex-col w-[50%] bg-red-500 ">
-        <div className="flex justify-between bg-green-600 w-full px-2 py-3">
+    <div className="w-full h-[270px] flex justify-center bg-[#E4E7EC]">
+      <div className="flex flex-col w-[50%]">
+        <div className="flex justify-between w-full ">
           <span className="font-bold text-sm">Recent Kicks</span>
           <span className="text-sm">
             <Link>View All</Link>
           </span>
         </div>
         {/* Slider */}
-        <div className="flex justify-evenly bg-yellow-600 h-full px-2 py-3">
+        <div className="flex justify-evenly h-full">
           <Carousel
             responsive={responsive}
-            containerClass={`w-full`}
-            swipeable={false}
-            draggable={false}
-            showDots={true}
+            containerClass={`w-full h-full`}
           >
             {[12, 3, 334, 434, 5434, 453].map(() => (
-              <div className="h-full bg-teal-800 rounded-3xl">
-                <div className="w-full h-full bg-teal-800 rounded-3xl">
+              <div className="w-[80%] h-full  rounded-3xl">
+                <div className="w-full h-full rounded-3xl">
                   <img
-                    src="./images/events.jpg"
+                    src="./images/diwali.jpg"
                     alt=""
                     className="w-full h-full rounded-3xl"
                   />
 
                   <img
-                    src="./images/events.jpg"
+                    src="./images/pizza.jpg"
                     alt=""
                     className="w-10 h-10 relative bottom-12 left-2 rounded-full"
                   />
                 </div>
-                <span className="w-[20%]"></span>
-                <span className="w-[20%]"></span>
-                <span className="w-[20%]"></span>
+               
               </div>
             ))}
           </Carousel>
-          ;
+         
         </div>
       </div>
     </div>
